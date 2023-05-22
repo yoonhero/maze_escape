180406cffc8ef3f37a92b80a2319f029be69b3a8
yoonhero <yoonhero06@naver.com>
2023-05-22 10:02:32 +0900

    feat: Broadcast

--- socket_stream.py
+++ socket_stream.py
@@ -10,9 +10,9 @@ from PIL import Image
 import math
 import os 
 
-from motor import Motor
+# from motor import Motor
 
-rcdriver = Motor((5, 6, 13), (16, 26, 12))
+# rcdriver = Motor((5, 6, 13), (16, 26, 12))
 velocity = 20
 
 app = Flask(__name__)
@@ -24,6 +24,10 @@ camera = cv2.VideoCapture(0)
 @app.route('/')
 def index():
 	return render_template('improved.html')
+
+@app.route("/live")
+def live():
+    return render_template("live.html")
 
 def img_to_base64_str(img):
     buffered = BytesIO()
@@ -58,8 +62,8 @@ def send():
     if not success:
         return
 
-    flip = cv2.flip(frame, 180)
-    retval, buffer = cv2.imencode(".jpeg", img=flip)
+    # flip = cv2.flip(frame, 180)
+    retval, buffer = cv2.imencode(".jpeg", img=frame)
     # jpg_as_base64 = base64.b64encode(buffer)
     image_base64 = "data:image/jpeg;base64," + base64.b64encode(buffer).decode('utf-8')
 
@@ -70,36 +74,36 @@ def send():
     # byte_img = img_to_base64_str(pil_image)
 
     # print(type(jpg_as_base64))
-    emit("video", {"data": image_base64})
+    emit("video", {"data": image_base64}, broadcast=True)
     # time.sleep(0.01)
 
-@socketio.event(namespace="/socket")
-def control(data):
-    direction = data.dir
-    angle = int(data.angle)
+# @socketio.event(namespace="/socket")
+# def control(data):
+#     direction = data.dir
+#     angle = int(data.angle)
     
-    x_axis = ["F", "L"]
-    y_axis = ["L", "R"]
+#     x_axis = ["F", "L"]
+#     y_axis = ["L", "R"]
 
-    t_vel = 0
-    if direction in x_axis:
-        t_vel = int(velocity + math.log10(angle+10) * 5)
-    elif direction in y_axis:
-        t_vel = int(velocity + math.log10(angle+10) * 3)
+#     t_vel = 0
+#     if direction in x_axis:
+#         t_vel = int(velocity + math.log10(angle+10) * 5)
+#     elif direction in y_axis:
+#         t_vel = int(velocity + math.log10(angle+10) * 3)
 
-    try: 
-        if direction == "F":
-            rcdriver.forward(velocity=t_vel)
-        elif direction == "B":
-            rcdriver.back(velocity=t_vel)
-        elif direction == "L":
-            rcdriver.left(velocity=t_vel)
-        elif direction == "R":
-            rcdriver.right(velocity=t_vel)
-        else:
-            rcdriver.stop_all()
-    except: 
-        return 
+#     try: 
+#         if direction == "F":
+#             rcdriver.forward(velocity=t_vel)
+#         elif direction == "B":
+#             rcdriver.back(velocity=t_vel)
+#         elif direction == "L":
+#             rcdriver.left(velocity=t_vel)
+#         elif direction == "R":
+#             rcdriver.right(velocity=t_vel)
+#         else:
+#             rcdriver.stop_all()
+#     except: 
+#         return 
 
 if __name__ == '__main__':
     socketio.run(app, host="127.0.0.1", port=8000)
